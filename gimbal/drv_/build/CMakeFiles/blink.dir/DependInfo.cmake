1f269838a6dfc17a7bb762b3f7c6eac8aefe1768
Rafael <rafa.grigorian@gmail.com>
2018-02-13 17:16:33 +0400

    builtttt

---
+++ gimbal/drv_/build/CMakeFiles/blink.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/muffin/Github/settings/gimbal/drv_/ADC.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/ADC.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/KALMAN.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/KALMAN.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/MPU6050.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/MPU6050.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/PID.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/PID.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/TIMER.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/TIMER.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/USART.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/USART.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/Variable.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/Variable.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/defines.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/defines.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/functions.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/functions.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/main.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/main.c.obj"
+  "/home/muffin/Github/settings/gimbal/drv_/twimastertimeout.c" "/home/muffin/Github/settings/gimbal/drv_/build/CMakeFiles/blink.dir/twimastertimeout.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "F_CPU=16000000"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
